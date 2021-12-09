114df4dd69f36969a1b6371a7516268e01359a1a
775pl <selma.eljabri1@gmail.com>
2021-12-09 12:01:24 +0100

    Ajout des boutons de zone et début de la configuration

--- app/src/main/java/fr/epsi/android/MainActivity.kt
+++ app/src/main/java/fr/epsi/android/MainActivity.kt
@@ -1,11 +1,26 @@
 package fr.epsi.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonZone1 = findViewById<Button>(R.id.button_main_zone1)
+        val buttonZone2 = findViewById<Button>(R.id.button_main_zone2)
+
+        buttonZone1.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application,Zone1Activity::class.java)
+            startActivity(newIntent)
+        })
+        buttonZone2.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application,Zone2Activity::class.java)
+            startActivity(newIntent)
+        })
     }
 }
