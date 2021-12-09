70a6531b71cf884d6fb3ac55ebcc0208bc77cf1e
775pl <selma.eljabri1@gmail.com>
2021-12-09 12:00:25 +0100

    Ajout des boutons de zone et début de la configuration

---
+++ app/src/main/java/fr/epsi/android/Zone1Activity.kt
@@ -0,0 +1,11 @@
+package fr.epsi.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Zone1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zone1)
+    }
+}
